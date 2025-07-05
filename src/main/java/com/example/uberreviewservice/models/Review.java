06810c92339d0eb51fcb28b4a9de2ff4f2ca0325
Divs-Git <divyansh.sri258@gmail.com>
2025-07-05 22:03:34 +0530

    added more columns for booking review table

--- src/main/java/com/example/uberreviewservice/models/Review.java
+++ src/main/java/com/example/uberreviewservice/models/Review.java
@@ -1,11 +1,32 @@
 package com.example.uberreviewservice.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
 
 @Entity
+@Table(name = "booking_review")
 public class Review {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @Column(name = "review_content", nullable = false)
+    String reviewContent;
+
+    @Column(name = "rating")
+    Double rating;
+
+    @Column(name = "created_at",nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate // this annotation tells that only handle it for object creation
+    Date createdAt;
+
+    @Column(name = "updated_at",nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate // this annotation tells that only handle it for object update
+    Date updatedAt;
 }
